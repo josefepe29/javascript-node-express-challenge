5e85dea122e04358f2e334dee9557f531bb3dd26
Jose Ferney Perez Camacho <josefepe29@gmail.com>
2024-03-01 11:39:35 -0500

    Fix MongoDB Atlas

--- server/db.js
+++ server/db.js
@@ -5,13 +5,10 @@ env.config();
 //Connection to the database
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://josefepe29:josefepe2890@cluster0.dtni1nd.mongodb.net/BusCompany?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connection to the database completed");
   } catch (error) {
     console.log(error);
