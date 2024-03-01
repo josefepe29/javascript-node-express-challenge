f900d86fbe142c52ade4cf44572b5e950a3d22c1
Jose Ferney Perez Camacho <josefepe29@gmail.com>
2024-03-01 11:19:53 -0500

    URI mongo DB Atlas

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const { validateHTTPMethods } = require("./auth/authentication.js");
 connectDB();
 
 //Environment Variables
-const PORT = process.env.PORT;
+const PORT = process.env.PORT | 3000;
 
 app.use(express.json());
 
--- server/db.js
+++ server/db.js
@@ -5,10 +5,13 @@ env.config();
 //Connection to the database
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://josefepe29:josefepe2890@cluster0.dtni1nd.mongodb.net/BusCompany?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("Connection to the database completed");
   } catch (error) {
     console.log(error);
